5122cbf61f0782216d486ea5db027118db4ad13e
Manar-Mobarke <81156136+Manar-Mobarke@users.noreply.github.com>
2023-04-02 19:33:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// const n = prompt("number=")
+// function fib(n){
+//     if(n==0 || n==1){
+// 		return n;
+// 	}
+	
+// 	else{
+// 		return fib(n-1)+fib(n-2);
+// 	}
+
+// }
+
+// console.log(fib(n))
+
+// function fibonacci() {
+//     let x = 0;
+//     let y = 1;
+//     let z = 0;
+//     return function getNext() {
+//         [z, x, y] = [x, y, x + y];
+//         return z;
+//     };
+// }
+
+// let fun = fibonacci();
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(fun());
+// }
